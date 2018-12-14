65800d862adb9277920daf249dc2df54a0b99d87
Eric Wang <30314019+AnichWang@users.noreply.github.com>
2018-12-14 18:35:42 +0800

    rewrite origin version

--- com/tool/sqlite_connect.py
+++ com/tool/sqlite_connect.py
@@ -1,10 +1,14 @@
 import sqlite3
 
+class sqliteConn:
+    pass
+
 if __name__ == "__main__":
     #连接数据库，如不存在，则在当前目录创建
     conn = sqlite3.connect('BlackMagic.db')
     cursor = conn.cursor()
 
+    """
     cursor.execute('create table order_info(id INT PRIMARY KEY NOT NULL,'
                    'cardNum varchar(20),'
                    'businessType INT,'
@@ -18,6 +22,21 @@ if __name__ == "__main__":
                    'favorFee varchar(20),'
                    'excelPayTime varchar(20),'
                    'excelCreated varchar(20))')
+    """
+
+    cursor.execute('insert into order_info values (1,'
+                   '\'1111\','
+                   '1,'
+                   '\'111\','
+                   '1,'
+                   '\'1111\','
+                   '\'111111\','
+                   '\'111111\','
+                   '\'11111111111\','
+                   '\'111111111111111\','
+                   '\'111111111111111\','
+                   '\'11111111111111111\','
+                   '\'1111111\')')
 
     cursor.close()
     conn.commit()
